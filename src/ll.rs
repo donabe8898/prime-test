670177c42d14eeeae158046bcc40c90d050b62fc
donabe8898 <60559703+donabe8898@users.noreply.github.com>
2023-09-01 21:55:21 +0900

    lucas lehmer ok
    Signed-off-by: donabe8898 <60559703+donabe8898@users.noreply.github.com>

--- src/ll.rs
+++ src/ll.rs
@@ -1,8 +1,10 @@
+use std::collections::btree_set::Intersection;
+
 use rand::Rng;
 use rug::Integer;
 
 pub fn is_prime_lucal_lehmer(p: Integer) -> bool {
-    if p <= Integer::from(1) {
+    if &p <= &Integer::from(1) || !(&p.get_bit(0)) {
         return false;
     }
 
@@ -10,17 +12,49 @@ pub fn is_prime_lucal_lehmer(p: Integer) -> bool {
         return true;
     }
 
+    let mut i = Integer::from(1);
+
+    loop {
+        let x: bool = gen_prime_num(&i);
+        if x {
+            let m = pw(&Integer::from(2), &i) - Integer::from(1);
+            if m == p {
+                return true;
+            }
+
+            if m > p {
+                return false;
+            }
+        }
+        i += Integer::from(1);
+    }
+}
+
+pub fn gen_prime_num(p: &Integer) -> bool {
     let mut s = Integer::from(4);
-    let m = Integer::from(2).pow_mod(&p, &Integer::from(1)).unwrap() - Integer::from(1);
+    let m = pw(&Integer::from(2), p) - Integer::from(1);
     let mut i = Integer::from(2);
 
     loop {
         s = (s.clone() * s.clone() - Integer::from(2)) % &m;
+
         i += Integer::from(1);
-        println!("{}", i);
-        if i > p {
+        if i >= *p {
             break;
         }
     }
     s == Integer::from(0)
+}
+
+pub fn pw(a: &Integer, b: &Integer) -> Integer {
+    let mut count: Integer = Integer::from(0);
+    let mut memory: Integer = Integer::from(1);
+    let res = loop {
+        if count == *b {
+            break memory;
+        }
+        memory *= a;
+        count += Integer::from(1);
+    };
+    res
 }
