2748e281e5d87e5a0d49c7acf2ca17917f5a0da1
donabe8898 <60559703+donabe8898@users.noreply.github.com>
2023-12-07 20:11:06 +0900

    30万
    Signed-off-by: donabe8898 <60559703+donabe8898@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ WARNING: 	注意が必要。
 
 fn main() {
     /*　生成個数 */
-    let piece = 70000;
+    let piece = 300000;
     let primes = [
         2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89,
         97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181,
@@ -33,72 +33,118 @@ fn main() {
     ];
 
     // 1024 bit
-    let test1024 = return_rands(1024, piece, primes);
+    let test1024: Vec<Integer> = return_rands(1024, piece, primes);
     println!("1024 OK!");
 
     // 2048 bit
-    let test2048 = return_rands(2048, piece, primes);
+    let test2048: Vec<Integer> = return_rands(2048, piece, primes);
     println!("2048 OK!");
 
     // 4096 bit
-    let test4096 = return_rands(4096, piece, primes);
+    let test4096: Vec<Integer> = return_rands(4096, piece, primes);
     println!("4096 OK!");
 
     /* NOTE: MR_MR計測 */
+    let mut safe_primes: Vec<Integer> = Vec::new();
+
     for tests in [&test1024, &test2048, &test4096] {
         let now: time::Instant = time::Instant::now();
         let mut test_miss = (0, 0, 0); // (1回目でmiss, 2回目でmiss, missなし)
         for test in tests {
             match test::mr_mr_bench(test.clone()) {
-                (false, false) => test_miss.2 += 1,
+                (false, false) => {
+                    safe_primes.push(test.clone());
+                    test_miss.2 += 1
+                }
                 (true, false) => test_miss.0 += 1,
                 (false, true) => test_miss.1 += 1,
                 (_, _) => {}
             }
         }
-        let end = now.elapsed();
+        let end: time::Duration = now.elapsed();
         println!(
             "\n ========= \nMR_MR\n time: {:?}, miss: {:?}\n========= ",
             end, test_miss
         );
     }
+
+    println!("safe_primes ===================");
+    if (!safe_primes.is_empty()) {
+        for safe_prime in safe_primes {
+            print!("-> ");
+            println!("{}", safe_prime);
+        }
+    }
+    println!("safe_primes end ===================");
+
     /* NOTE:  MR_EEL計測 */
+    let mut safe_primes: Vec<Integer> = Vec::new();
+
     for tests in [&test1024, &test2048, &test4096] {
         let now: time::Instant = time::Instant::now();
         let mut test_miss = (0, 0, 0); // (1回目でmiss, 2回目でmiss, missなし)
         for test in tests {
             match test::mr_eel_bench(test.clone()) {
-                (false, false) => test_miss.2 += 1,
+                (false, false) => {
+                    safe_primes.push(test.clone());
+                    test_miss.2 += 1
+                }
                 (true, false) => test_miss.0 += 1,
                 (false, true) => test_miss.1 += 1,
                 (_, _) => {}
             }
         }
-        let end = now.elapsed();
+        let end: time::Duration = now.elapsed();
         println!(
             "\n ========= \nMR_EEL\n time: {:?}, miss: {:?}\n========= ",
             end, test_miss
         );
     }
+
+    println!("safe_primes ===================");
+    if (!safe_primes.is_empty()) {
+        for safe_prime in safe_primes {
+            print!("-> ");
+            println!("{}", safe_prime);
+        }
+    }
+    println!("safe_primes end ===================");
+
     /* NOTE: EEL_MR計測 */
+    let mut safe_primes: Vec<Integer> = Vec::new();
+
     for tests in [&test1024, &test2048, &test4096] {
         let now: time::Instant = time::Instant::now();
         let mut test_miss = (0, 0, 0); // (1回目でmiss, 2回目でmiss, missなし)
         for test in tests {
             match test::eel_mr_bench(test.clone()) {
-                (false, false) => test_miss.2 += 1,
+                (false, false) => {
+                    safe_primes.push(test.clone());
+                    test_miss.2 += 1
+                }
                 (true, false) => test_miss.0 += 1,
                 (false, true) => test_miss.1 += 1,
                 (_, _) => {}
             }
         }
-        let end = now.elapsed();
+        let end: time::Duration = now.elapsed();
         println!(
             "\n ========= \nEEL_MR\n time: {:?}, miss: {:?}\n========= ",
             end, test_miss
         );
     }
+
+    println!("safe_primes ===================");
+    if (!safe_primes.is_empty()) {
+        for safe_prime in safe_primes {
+            print!("-> ");
+            println!("{}", safe_prime);
+        }
+    }
+    println!("safe_primes end ===================");
+
     /* NOTE: EEL_EEL計測 */
+    let mut safe_primes: Vec<Integer> = Vec::new();
 
     for tests in [&test1024, &test2048, &test4096] {
         let now: time::Instant = time::Instant::now();
@@ -111,12 +157,21 @@ fn main() {
                 (_, _) => {}
             }
         }
-        let end = now.elapsed();
+        let end: time::Duration = now.elapsed();
         println!(
             "\n ========= \nEEL_EEL\n time: {:?}, miss: {:?}\n========= ",
             end, test_miss
         );
     }
+
+    println!("safe_primes ===================");
+    if (!safe_primes.is_empty()) {
+        for safe_prime in safe_primes {
+            print!("-> ");
+            println!("{}", safe_prime);
+        }
+    }
+    println!("safe_primes end ===================");
 }
 
 fn return_rands(bits: u64, piece: usize, primes: [i32; 130]) -> Vec<Integer> {
